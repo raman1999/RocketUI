0f3f689c2c0b90b5ae76c8747d69afc8d93f14ab
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-02-11 13:30:11 -0800

    feat : added javascript  file

---
+++ js/script.js
@@ -0,0 +1,58 @@
+/** Documentation site navigation on small screen */
+const btnHamburger = document.querySelector("#btn-hamburger");
+const ListMain = document.querySelector("#main-list");
+const linkBtn = document.querySelectorAll(".btn-link");
+
+btnHamburger.addEventListener("click", () => {
+  ListMain.classList.toggle("nav-list-hide");
+  ListMain.classList.toggle("list-inline");
+});
+
+for (eachBtn of linkBtn) {
+  eachBtn.addEventListener("click", () => {
+    ListMain.classList.toggle("nav-list-hide");
+  });
+}
+
+/** Navigation component */
+const btnNav = document.querySelector("#btn-nav");
+const navList = document.querySelector("#list-nav");
+
+btnNav.addEventListener("click", () => {
+  navList.classList.toggle("nav-list-hide");
+});
+
+/** Modal */
+const modalContainer = document.querySelector(".modal-container");
+const btnModal = document.querySelector("#btn-modal");
+const btnClose = document.querySelector("#btn-close-modal");
+
+btnModal.addEventListener("click", () => {
+  modalContainer.style.display = "block";
+});
+
+btnClose.onclick = () => {
+  modalContainer.style.display = "none";
+};
+
+window.onclick = (event) => {
+  if (event.target == modalContainer) {
+    modalContainer.style.display = "none";
+  }
+};
+
+/** Toast */
+const toast = document.querySelector(".toast-container");
+const btnToast = document.querySelector("#btn-toast");
+const btnCloseToast = document.querySelector("#btn-close-toast");
+
+const closeToast = () => {
+  toast.style.display = "none";
+};
+
+btnToast.addEventListener("click", () => {
+  toast.style.display = "block";
+  setTimeout(closeToast, 3000);
+});
+
+btnCloseToast.onclick = closeToast;
