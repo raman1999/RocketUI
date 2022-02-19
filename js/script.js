c13e851aecf121ad8a2a24997a54961b9e151208
raman1999 <45732494+raman1999@users.noreply.github.com>
2022-02-19 13:50:14 -0800

    added slider html structure

--- js/script.js
+++ js/script.js
@@ -56,3 +56,13 @@ btnToast.addEventListener("click", () => {
 });
 
 btnCloseToast.onclick = closeToast;
+
+// Slider 
+var slider = document.querySelector('#slider')
+var label = document.querySelector('#slider-value')
+label.innerHTML = slider.value;
+slider.style.background = 'linear-gradient(90deg, var(--txtSecondary) '+slider.value/100+'%, var(--bgLight) 0%)'
+slider.oninput = function() {
+  label.innerHTML = slider.value;
+  slider.style.background = 'linear-gradient(90deg, var(--txtSecondary) '+slider.value/100+'%, var(--bgLight) 0%)'
+}
